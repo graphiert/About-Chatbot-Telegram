f02dfd081fefa028e4d74256386af0ba70c56c2e
galihpujiirianto <galihpujiirianto@gmail.com>
2022-06-12 15:24:51 +0700

    separating files

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from pyrogram.types import (
     InlineQueryResultArticle
 )
 from keyboard import *
+from utils import *
 from config import *
 
 if not OWNER_ID:
@@ -22,32 +23,6 @@ bot = Client(
 )
 
 chatbot = {}
-
-def send(message, photo, text, reply_markup):
-    def edit(message, text, reply_markup):
-        message.edit_message_text(
-            text=text,
-            reply_markup=reply_markup,
-            disable_web_page_preview=True
-        )
-    if IMG_START:
-        try:
-            message.reply_photo(
-                photo=photo,
-                caption=text,
-                reply_markup=reply_markup
-            )
-        except AttributeError:
-            edit(message, text, reply_markup)
-    else:
-        try:
-            message.reply_text(
-                text=text,
-                reply_markup=reply_markup,
-                disable_web_page_preview=True
-            )
-        except AttributeError:
-            edit(message, text, reply_markup)
 
 @bot.on_message(filters.command("start") & filters.private)
 @bot.on_callback_query(filters.regex(pattern=r"back"))
---
+++ utils.py
@@ -0,0 +1,25 @@
+def send(message, photo, text, reply_markup):
+    def edit(message, text, reply_markup):
+        message.edit_message_text(
+            text=text,
+            reply_markup=reply_markup,
+            disable_web_page_preview=True
+        )
+    if IMG_START:
+        try:
+            message.reply_photo(
+                photo=photo,
+                caption=text,
+                reply_markup=reply_markup
+            )
+        except AttributeError:
+            edit(message, text, reply_markup)
+    else:
+        try:
+            message.reply_text(
+                text=text,
+                reply_markup=reply_markup,
+                disable_web_page_preview=True
+            )
+        except AttributeError:
+            edit(message, text, reply_markup)
