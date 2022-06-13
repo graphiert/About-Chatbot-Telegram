bfab20b666c11b6eddcd808391cd1424018b32d4
galihpujiirianto <galihpujiirianto@gmail.com>
2022-06-13 15:59:47 +0700

    KENTUT!

---
+++ plugins/chatbot.py
@@ -0,0 +1,20 @@
+# @galihpujiirianto https://github.com/galihpujiirianto/About-Chatbot-Telegram
+
+from pyrogram import filters
+from utils import chatbot
+from config import OWNER_ID
+
+@bot.on_message(filters.private & ~filters.edited)
+def incoming_private(_, message):
+    user_id = message.from_user.id
+    if user_id == OWNER_ID:
+        sender = message.reply_to_message.forward_sender_name
+        for name, rep_uid in chatbot.items():
+            if name == sender:
+                message.forward(rep_uid, is_copy=True)
+    else:
+        if bot.get_me().id == message.from_user.id:
+            return
+        name = message.from_user.first_name
+        chatbot[name] = message.from_user.id
+        message.forward(OWNER_ID)
---
+++ plugins/inline.py
@@ -0,0 +1,48 @@
+# @galihpujiirianto https://github.com/galihpujiirianto/About-Chatbot-Telegram
+
+from pyrogram import filters
+from pyrogram.types import (
+    InputTextMessageContent,
+    InlineQueryResultArticle
+)
+from config import (
+    MSG_START, IMG_START, ONWER_ID,
+    ABOUT_MSG, PROJECTS_MSG, SOCMED_MSG
+)
+
+@bot.on_inline_query(filters.user(OWNER_ID))
+def inline_query(_, inline_query):
+   inline_query.answer(
+        results=[
+            InlineQueryResultArticle(
+                title="About Me!",
+                description="To describe myself.",
+                thumb_url="https://telegra.ph//file/e205a797b4599686909e2.jpg",
+                input_message_content=InputTextMessageContent(ABOUT_MSG)
+            ),
+            InlineQueryResultArticle(
+                title="My Projects!",
+                description="To show off my project.",
+                thumb_url="https://telegra.ph//file/e205a797b4599686909e2.jpg",
+                input_message_content=InputTextMessageContent(PROJECTS_MSG)
+            ),
+            InlineQueryResultArticle(
+                title="Connect with me!",
+                description="To show my social media.",
+                thumb_url="https://telegra.ph//file/e205a797b4599686909e2.jpg",
+                input_message_content=InputTextMessageContent(SOCMED_MSG)
+            )
+        ]
+    )
+
+@bot.on_inline_query(~filters.user(OWNER_ID))
+def notowner(_, inline_query):
+   inline_query.answer(
+        results=[
+            InlineQueryResultArticle(
+                title="You're not allowed!",
+                description="This can only work by bot owner.",
+                input_message_content=InputTextMessageContent("No. You aren't allowed to accessed this bot using inline.")
+            )
+        ]
+    )
